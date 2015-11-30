657368bbc070e1be8cc3689bcb613f8301d30545
Noah Adler <noah.adler@gmail.com>
2015-11-30 13:50:21 -0500

    Make sure package.js can find files to run tests.

--- package.js
+++ package.js
@@ -36,7 +36,7 @@ Package.onTest(function (api) {
 
   api.addFiles([
     'lib/filters.coffee',
-    'client/helpers/helpers.coffee',
+    'client/components/helpers/helpers.coffee',
     'tests/facets-test.coffee',
     'tests/parsers-test.coffee',
     'tests/email-function-tests.coffee',
